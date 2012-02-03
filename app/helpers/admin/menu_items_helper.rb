3cd03dd721bba3ddbcbfe9b9967b9403bb4992cd
Nathan Wise <nathanwise@gmail.com>
2012-02-03 17:44:41 -0500

    styling menu items

--- app/helpers/admin/menu_items_helper.rb
+++ app/helpers/admin/menu_items_helper.rb
@@ -5,11 +5,16 @@ module Admin::MenuItemsHelper
     menu_item.children.each do |child|
       #html += "<li><a href='#{child.url}'>#{child.label}</a>"
       html += "<li>#{child.label}"
-      html += link_to(image_tag('admin_icons/add.png'), new_admin_menu_item_path(:parent_id => child.id) ) 
-      html += link_to(image_tag('admin_icons/edit.png'), edit_admin_menu_item_path(child)) 
-      html += link_to(image_tag('admin_icons/delete.png'), admin_menu_item_path(child), 
-                                                            :confirm => 'Are you sure?', 
-                                                            :method => :delete)
+      html += link_to("<i class='icon-play-circle icon-white'></i>&nbsp; Create Submenu".html_safe,
+                  new_admin_menu_item_path(:parent_id => child.id),
+                  :id => "#{child.id}_add_child",
+                  :title => 'add-child',
+                  :class => 'btn btn-success') 
+      html += link_to("<i class='icon-edit icon-white'></i>&nbsp; Edit Submenu".html_safe,
+                  edit_admin_menu_item_path(child), :class => 'btn btn-primary')
+      html += link_to("<i class='icon-trash icon-white'></i>&nbsp; Delete Submenu".html_safe, admin_menu_item_path(child),
+                                                      :confirm => 'Are you sure?',
+                                                      :method => :delete, :class => 'btn btn-warning') 
       html += "</li>"
       unless child.children.empty?
         html += '<ul>' + draw_menu_tree(child) + '</ul>'        
