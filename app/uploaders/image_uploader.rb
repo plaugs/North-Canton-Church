d008588eb0a670035ccdf2a247b91377b011251e
Nathan Wise <nathanwise@gmail.com>
2012-02-25 14:04:28 -0500

    Adding image listing to page admin

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,6 +36,10 @@ class ImageUploader < CarrierWave::Uploader::Base
    version :medium do
      process :resize_to_fit => [200, 200]
    end
+   
+   version :large do
+     process :resize_to_fit => [600, 600]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
