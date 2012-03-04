162385092213339140c843593abeaddc58a0a4af
Nathan Wise <nathanwise@gmail.com>
2012-03-04 12:53:39 -0500

    Adding xlarge to uploader.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,10 @@ class ImageUploader < CarrierWave::Uploader::Base
    version :large do
      process :resize_to_fit => [600, 600]
    end
+   
+   version :xlarge do
+     process :resize_to_fit => [800,800]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
