864c1366deb8fa0e0286c1b402bf7553670a94b1
Nathan Wise <nathanwise@gmail.com>
2013-01-20 14:31:09 -0500

    getting fog working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Northcantonchurch::Application.configure do
       :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']                    # required
-    config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+    #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
